7e67541264be9501ed13d87870442f3a17f04a1f
Ilkhomjon Umaraliev <umaralievilkhomjon28@gmail.com>
2024-04-20 21:01:30 +0500

    Added some changes

--- quiz-app-test/script.js
+++ quiz-app-test/script.js
@@ -1,14 +1,75 @@
 // Start of APP QUIZ DATA
 
 const questions = [
+  //   ***************************************************************
   {
-    question: "Eshitmoq (Слушать)",
+    question: "Barmoq bilan bosish (нажимать)",
     answers: [
-      { text: "Talk", correct: false },
-      { text: "Listen", correct: true },
-      { text: "Jump", correct: false },
+      { text: "Rub", correct: false },
+      { text: "Tap", correct: true },
+      { text: "Pluck", correct: false },
     ],
   },
+  //   ****************************************************************
+  //   ***************************************************************
+  {
+    question: "Portlash (взрываться)",
+    answers: [
+      { text: "Explode", correct: true },
+      { text: "Slam", correct: false },
+      { text: "Pluck", correct: false },
+    ],
+  },
+  //   ****************************************************************
+  //   ***************************************************************
+  {
+    question: "Bermoq  (давать)",
+    answers: [
+      { text: "Close", correct: false },
+      { text: "Give", correct: true },
+      { text: "Play", correct: false },
+    ],
+  },
+  //   ****************************************************************
+
+  {
+    question: "O’qimoq (Изучать)",
+    answers: [
+      { text: "Sleep", correct: false },
+      { text: "Study", correct: true },
+      { text: "Think", correct: false },
+    ],
+  },
+  //   ***************************************************************
+  //   ***************************************************************
+  {
+    question: "Tozalamoq (чистить)",
+    answers: [
+      { text: "Think", correct: false },
+      { text: "Study", correct: false },
+      { text: "Clean", correct: true },
+    ],
+  },
+  //   ****************************************************************
+  {
+    question: "Baqirmoq (кричать)",
+    answers: [
+      { text: "Shout", correct: true },
+      { text: "Explode", correct: false },
+      { text: "Sneeze", correct: false },
+    ],
+  },
+  //   ****************************************************************
+  //   ***************************************************************
+  {
+    question: "Uyg’onmoq  (разбудить)",
+    answers: [
+      { text: "Smell", correct: false },
+      { text: "Fly", correct: false },
+      { text: "Wake", correct: true },
+    ],
+  },
+  //   ****************************************************************
   {
     question: "Sirpanish (скользить)",
     answers: [
@@ -17,6 +78,46 @@ const questions = [
       { text: "Bow", correct: false },
     ],
   },
+  //   ***************************************************************
+  //   ***************************************************************
+  {
+    question: "O’tirmoq (Сидеть)",
+    answers: [
+      { text: "Sit", correct: true },
+      { text: "Read", correct: false },
+      { text: "Write", correct: false },
+    ],
+  },
+  //   ****************************************************************
+  {
+    question: "Qaltirash (дрожать)",
+    answers: [
+      { text: "Shiver", correct: true },
+      { text: "Frown", correct: false },
+      { text: "Pout", correct: false },
+    ],
+  },
+  //   ****************************************************************
+  //   ***************************************************************
+  {
+    question: "To’satdan sinish, Buzilish (щелкнуть)",
+    answers: [
+      { text: "Clap", correct: false },
+      { text: "Slam", correct: false },
+      { text: "Snap", correct: true },
+    ],
+  },
+  //   ****************************************************************
+  //   ***************************************************************
+  {
+    question: "Chuqur nafas olish “uff” tortish (вздыхать)",
+    answers: [
+      { text: "Shrug", correct: false },
+      { text: "Cough", correct: false },
+      { text: "Sigh", correct: true },
+    ],
+  },
+  //   ****************************************************************
   {
     question: "Ta’zim qilish (кланяться)",
     answers: [
@@ -25,6 +126,26 @@ const questions = [
       { text: "Bow", correct: true },
     ],
   },
+  //   ***************************************************************
+  //   ***************************************************************
+  {
+    question: "Yozmoq (Писать)",
+    answers: [
+      { text: "Dance", correct: false },
+      { text: "Write", correct: true },
+      { text: "Sing", correct: false },
+    ],
+  },
+  //   ****************************************************************
+  {
+    question: "Tushurib yubormoq  (уронить)",
+    answers: [
+      { text: "Throw", correct: false },
+      { text: "Drop", correct: true },
+      { text: "Catch", correct: false },
+    ],
+  },
+  //   ****************************************************************
   {
     question: "Bosh silkitish (кивать)",
     answers: [
@@ -50,6 +171,16 @@ const questions = [
       { text: "Spin", correct: false },
       { text: "Roll", correct: false },
       { text: "Shake", correct: true },
+    ],
+  },
+  //   ****************************************************************
+  //   ***************************************************************
+  {
+    question: "Yo’talish (кашлять)",
+    answers: [
+      { text: "Sneeze", correct: false },
+      { text: "Cough", correct: true },
+      { text: "Hunch", correct: false },
     ],
   },
   //   ****************************************************************
@@ -247,17 +378,18 @@ const questions = [
       { text: "Laughing", correct: true },
     ],
   },
-  //   ****************************************************************
-  //   ***************************************************************
   {
-    question: "O’qimoq (Изучать)",
+    question: "Hurrak otish (фыркать)",
     answers: [
-      { text: "Sleep", correct: false },
-      { text: "Study", correct: true },
-      { text: "Think", correct: false },
+      { text: "Explode", correct: false },
+      { text: "Snort", correct: true },
+      { text: "Slam", correct: false },
     ],
   },
   //   ****************************************************************
+  //   ****************************************************************
+  //   ***************************************************************
+
   //   ***************************************************************
   {
     question: "Tozalamoq (чистить)",
@@ -265,6 +397,47 @@ const questions = [
       { text: "Clean", correct: true },
       { text: "Cook", correct: false },
       { text: "Think", correct: false },
+    ],
+  },
+  //   ****************************************************************
+
+  //   ***************************************************************
+  {
+    question: "Pichirlab gapirish (шептать)",
+    answers: [
+      { text: "Scream", correct: false },
+      { text: "Shout", correct: false },
+      { text: "Whisper", correct: true },
+    ],
+  },
+  //   ****************************************************************
+  //   ***************************************************************
+  //   ****************************************************************
+  {
+    question: "Eshitmoq (Слушать)",
+    answers: [
+      { text: "Talk", correct: false },
+      { text: "Listen", correct: true },
+      { text: "Jump", correct: false },
+    ],
+  },
+  //   ***************************************************************
+  {
+    question: "Bukchayib o’tirish (горбиться)",
+    answers: [
+      { text: "Shrug", correct: false },
+      { text: "Sigh", correct: false },
+      { text: "Hunch", correct: true },
+    ],
+  },
+  //   ****************************************************************
+  //   ***************************************************************
+  {
+    question: "Uzish, Yulish",
+    answers: [
+      { text: "Pluck", correct: true },
+      { text: "Rub", correct: false },
+      { text: "Tap", correct: false },
     ],
   },
   //   ****************************************************************
